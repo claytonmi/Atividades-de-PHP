44076acbd18f6c279627825049d4c8e6b7d0b079
Clayton Machado Ilibio <32619055+claytonmi@users.noreply.github.com>
2019-09-04 21:32:37 -0300

    ajuste

--- ListaAtividade/Urna/index.php
+++ ListaAtividade/Urna/index.php
@@ -4,19 +4,16 @@ require_once "Candidato.php";
 require_once "Voto.php";
 require_once "Urna.php";
 
-$Canditado = new Candidato(17, "Jose Paulo", 17);
-$Canditado2 = new Candidato(13, "Paulinho", 13);
+$Canditado = new Candidato(17, "Jose Paulo", 1);
+$Canditado2 = new Candidato(13, "Paulinho", 1);
 
-$Voto = new Voto($Canditado);
-$Voto2 = new Voto($Canditado2);
-
-$Urna = new Urna(1, 10, $Voto);
-$Urna->addVotos($Voto);
-$Urna->addVotos($Voto);
-$Urna->addVotos($Voto);
-$Urna->addVotos($Voto);
-$Urna->addVotos($Voto2);
-$Urna->addVotos($Voto2);
-$Urna->addVotos($Voto2);
+$Urna = new Urna(1, 10);
+$Urna->addVotos($Canditado);
+$Urna->addVotos($Canditado);
+$Urna->addVotos($Canditado);
+$Urna->addVotos($Canditado);
+$Urna->addVotos($Canditado2);
+$Urna->addVotos($Canditado2);
+$Urna->addVotos($Canditado2);
 
 $Urna->relatorio();
